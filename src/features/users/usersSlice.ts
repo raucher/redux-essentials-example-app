b1f895ea395f2627233520df78de72016d1bf1af
maksims <maksims.muizininks@gmail.com>
2025-01-21 16:37:00 +0100

    fetching users at startup

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,24 +1,31 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import type { RootState } from '@/app/store'
 
 import { selectCurrentUsername } from '@/features/auth/authSlice'
+import { client } from '@/api/client'
+import { createAppAsyncThunk } from '@/app/withTypes'
+import { create } from 'domain'
 
 interface User {
   id: string
   name: string
 }
 
-const initialState: User[] = [
-  { id: '0', name: 'Tianna Jenkins' },
-  { id: '1', name: 'Kevin Grant' },
-  { id: '2', name: 'Madison Price' },
-]
+export const fetchUsers = createAppAsyncThunk('/users/fetchUsers', async () => {
+  const response = await client.get<User[]>('/fakeApi/users')
+  return response.data
+})
+
+const initialState: User[] = []
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => action.payload)
+  },
 })
 
 export default usersSlice.reducer
